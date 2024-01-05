23214d38be0cfe9214eacbc848f7ae76f6ab34e0
Shasten <shastenm76@gmail.com>
2024-01-04 20:49:02 -0700

    updating lua script

---
+++ nnn.lua
@@ -0,0 +1,23 @@
+
+local M = {}
+
+function M.open_nnn_browser()
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    -- Run nnn in a terminal window
+    vim.fn.termopen('nnn', { cwd = vim.fn.getcwd(), on_exit = function() vim.api.nvim_buf_delete(buf, { force = true }) end })
+
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = 'editor',
+        width = 40,
+        height = 10,
+        row = 10,
+        col = 10,
+        style = 'minimal',
+        border = 'single',
+    })
+
+    return { buf = buf, win = win }
+end
+
+return M
